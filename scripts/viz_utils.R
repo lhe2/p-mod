162bf02c1361a6a371c65d414031839399152a67
lhe2 <97200903+lhe2@users.noreply.github.com>
2026-05-28 15:12:06 -0400

    feat: updating tidying workflow and in-progress mod viz/figs for 250627 lab mtg
    - update dev trajectories; tdt exit time, mass change figs
    - update wrangling per mid-cohort B data collection updates (more instars, new filters/update dev binary assignments)
    - start working on wrangling for dev outcomes bar plot
    - add viz utils

---
+++ scripts/viz_utils.R
@@ -0,0 +1,13 @@
+# mod 2026 viz utils
+# 2026-05-26
+
+## SCALE OBJECTS ##
+p_scales <- list(
+  lab_maxT = "maximum\ntemperature (°C)",
+  lab_flucT = "fluctuation (±°C)",
+  
+  facs_trt = c(`319` = "33-19°C\n(26±7°C)",
+               `326` = "33-26°C\n(29.5±3.5°C)",
+               `426` = "40-26°C\n(33±7°C)",
+               `433` = "40-33°C\n(36.5±3.5°C)")
+)
--- scripts/wrangle.R
+++ scripts/wrangle.R
@@ -6,37 +6,12 @@
 ## load math fns
 source(here("scripts/math_utils.R"))
 
-
-# tdt summaries ---------------------------------------------------------------
-
-PrepTDTSS <- function(wide_df){
-  wide_df %>%
-    #filter(trt.type == "tdt") %>% 
-    select(c("cohort", starts_with("trt"), contains(c("pmd", "0h", "24h", "48h")))) %>%
-    pivot_longer(cols = ends_with(c("0h", "24h", "48h")),
-                 names_to = c(".value", "instar"),
-                 names_sep = "\\.") %>%
-    rename(exit = tt.pmd) %>%
-    group_by(across(c(starts_with("trt"), "instar", #"cohort" 
-                      ))) %>% 
-    summarise(n = n(),
-              across(.cols = c(mass, dmass, rmass, exit),
-                     .fns = list(avg = ~ mean(.x, na.rm = TRUE),
-                                 se = se),
-                     .names = "{.fn}.{.col}")
-              # avg.delta = mean(mass.change, na.rm = TRUE),
-              # se.delta = se(mass.change),
-              # avg.exit = mean(tt.pmd, na.rm = TRUE),
-              # se.exit = se(tt.pmd)
-              ) %>%
-    ungroup()
-}
-
 # dev summaries -----------------------------------------------------------
 
-PrepDevSS <- function(long_df){
+CalcDevSS <- function(long_df){
   long_df %>%
-    group_by(across(c(starts_with("trt"), instar))) %>%
+    filter(trt.type == "dev") %>%
+    group_by(across(c(starts_with("trt"), instar, is.sup))) %>%
     mutate(logmass = log(mass),
            # mass = case_when(instar %in% c("pupa", "eclose") ~ mass/1000,
            #                  TRUE ~ mass),
@@ -51,12 +26,88 @@ PrepDevSS <- function(long_df){
     ungroup()
 }
 
-PrepSurvSS <- function(wide_df){
+CalcSurvSS <- function(wide_df){
   wide_df %>%
+    filter(trt.type == "dev") %>%
     group_by(across(starts_with("trt"))) %>%
     summarise(n = n(),
-              prop.pup = sum(is.pup == 1)/n,
+              prop.pup = sum(is.pup > 0)/n,
               se.pup = seprop(prop.pup, n)) %>%
     ungroup()
+}
+
+CalcOutcomesSS <- function(wide_df){
+  wide_df %>%
+    filter(trt.type == "dev",
+           !is.na(is.pup) # omit in-progress bugs
+           ) %>%
+    group_by(across(starts_with(c("trt")))) %>%
+    summarise(tot_N = n(),
+              N_pmd = sum(is.pup == 0 #& is.sup == 0
+                          , na.rm = TRUE),
+              # TODO fix wonky counting for the N_pup/sup..
+              N_pup = sum(is.pup > 0 & is.sup == 0, na.rm = TRUE),
+              N_sup = sum(is.pup > 0 & is.sup == 1, na.rm = TRUE),
+              tot_sup = sum(is.sup == 1, na.rm = TRUE),
+              sup_pmd = sum(is.sup == 1 & is.pup == 0, na.rm = TRUE),
+              sup_pup = sum(is.sup == 1 & is.pup == 1, na.rm = TRUE),
+              sup_LPI = sum(is.sup == 1 & is.pup == 2, na.rm = TRUE),
+              ) %>%
+    pivot_longer(cols = starts_with(c("N_", "sup_")),
+                 names_to = c("type", "outcome"),
+                 values_to = "n",
+                 names_sep = "_") %>%
+    mutate(prop = case_when(type == "N" ~ n/tot_N,
+                            type == "sup" ~ n/tot_sup),
+           type = factor(type, levels = c("N", "sup")),
+           outcome = factor(outcome, levels = c("sup", "pup", "LPI", "pmd")))
     
+    # ## this is what i envision, count-wise
+    # group_by(across(starts_with(c("trt", "is.")))) %>%
+    # summarise(N = n()) %>%
+    # drop_na(is.pup) 
+    
+    # ## old...
+    # group_by(across(starts_with(c("trt", "is.")))) %>%
+    # summarise(N_all = n(),
+    #           N_pmd = sum(is.pup == 0, na.rm = TRUE),
+    #           not_sup = sum(is.sup == 0, na.rm = TRUE),
+    #           is_sup = sum(is.sup == 1, na.rm = TRUE),
+    #           is_pup = sum(is.pup == 1, na.rm = TRUE),
+    #           is_LPI = sum(is.LPI == 1, na.rm = TRUE),
+    #           ) %>%
+    # ungroup() %>%
+    # drop_na(is.pup) #%>%
+    # # pivot_longer(cols = starts_with(c("not_", "is_")),
+    # #              names_to = c("status", "outcome"),
+    # #              values_to = "n",
+    # #              names_sep = "_")
+}
+
+
+# tdt summaries ---------------------------------------------------------------
+
+CalcTDTSS <- function(wide_df){
+  wide_df %>%
+    # TODO filter trt.type here or elsewhere?
+    filter(trt.type == "tdt") %>% 
+    select(c("cohort", starts_with("trt"), contains(c("pmd", "0h", "24h", "48h")))) %>%
+    pivot_longer(cols = ends_with(c("0h", "24h", "48h")),
+                 names_to = c(".value", "instar"),
+                 names_sep = "\\.") %>%
+    rename(exit = tt.pmd) %>%
+    # TODO group by cohort?
+    group_by(across(c(starts_with("trt"), "instar", #"cohort" 
+    ))) %>% 
+    summarise(n = n(),
+              across(.cols = c(mass, dmass, rmass, exit),
+                     .fns = list(avg = ~ mean(.x, na.rm = TRUE),
+                                 se = se),
+                     .names = "{.fn}.{.col}")
+              # avg.delta = mean(mass.change, na.rm = TRUE),
+              # se.delta = se(mass.change),
+              # avg.exit = mean(tt.pmd, na.rm = TRUE),
+              # se.exit = se(tt.pmd)
+    ) %>%
+    ungroup()
 }
